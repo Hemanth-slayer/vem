c07244d0d5d6e1c22b5eca62590b86b5e70786da
Hemanth/oxyslayer <118145147+Hemanth-slayer@users.noreply.github.com>
2024-05-28 19:53:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-var video = document.querySelector("#videoplayer");
-
-function stream(videoElement) {
-  if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(function(mediaStream) {
-        videoElement.srcObject = mediaStream;
-        videoElement.play();
-      })
-      .catch(function(error) {
-        console.error("Error accessing the device camera:", error);
-      });
-  } else {
-    console.error("getUserMedia not supported in this browser.");
-  }
-}
-
-// Call the stream function passing the video element
-stream(video);
+ <button id="startButton">Start Camera</button>
+  <video id="video" autoplay></video>
+  <script src="script.js"></script>
